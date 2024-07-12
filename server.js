abd0d118e7da14d13c56cdd0f62d99e7705d604a
jlimsy <limsy.joey@gmail.com>
2024-07-13 01:07:13 +0800

    install tailwind

---
+++ server.js
@@ -0,0 +1,29 @@
+// Require the installed modules for Express
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+// configure databse
+require("dotenv").config();
+require("./config/database");
+
+// Start the express server
+const app = express();
+
+// middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+// API routes
+
+// Catch all
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      {" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+}
+
+export default App;
