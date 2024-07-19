1de9ca6c6602f7140bd29ca23957d1ddf13b6199
jlimsy <limsy.joey@gmail.com>
2024-07-19 08:01:00 +0800

    style auth page

--- src/pages/Auth/AuthPage.jsx
+++ src/pages/Auth/AuthPage.jsx
@@ -6,14 +6,17 @@ export default function AuthPage({ setUser }) {
   const [isNewUser, setIsNewUser] = useState(false);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="mx-auto w-full flex-col max-w-sm">
-        {isNewUser ? (
-          <SignUpForm setIsNewUser={setIsNewUser} />
-        ) : (
-          <LoginForm setUser={setUser} setIsNewUser={setIsNewUser} />
-        )}
+    <div className="grid grid-cols-3">
+      <div className="col-span-1 flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="mx-auto w-full max-w-sm">
+          {isNewUser ? (
+            <SignUpForm setIsNewUser={setIsNewUser} />
+          ) : (
+            <LoginForm setUser={setUser} setIsNewUser={setIsNewUser} />
+          )}
+        </div>
       </div>
+      <div className="col-span-2 bg-[url('/public/fruit.jpg')] bg-cover bg-bottom"></div>
     </div>
   );
 }
