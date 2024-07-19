9a2e2abc99f823bb376c08c93284270bd8132201
joeylim <joeylim@cbm.com.sg>
2024-07-19 19:13:36 +0800

    style: change colour of havbar

---
+++ src/components/Owner/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import YearlySalesChart from "./YearlySalesChart";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <YearlySalesChart />
+    </div>
+  );
+}
---
+++ src/pages/Owner/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import Dashboard from "@/components/Owner/Dashboard/Dashboard";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
